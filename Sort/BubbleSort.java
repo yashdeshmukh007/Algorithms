1a32bb663851774cc9580ce9ab78b48ba7ffd8e9
Yash Deshmukh <yashdeshmukh.competition@gmail.com>
2021-10-01 21:01:57 +0530

    BubbleSort Is Completed

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,35 @@
+package Sort;
+import java.util.Arrays;
+public class BubbleSort {
+    public static void main(String args[])
+    {
+        int arr[]={5,4,3,2,1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr)
+    {
+        
+        for(int i=0;i<arr.length;i++)
+        {
+           
+            for(int j=1;j<arr.length-i;j++)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    
+                }
+            }
+            /*if(c==0)
+            {
+                break;
+            }*/
+        }
+
+    }
+
+}
